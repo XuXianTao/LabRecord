aeaeeefd88cd5c79cbbfb5eab96ba9299c8a6b31
WuRui <261857860@qq.com>
2018-05-23 21:57:08 +0800

    updt

---
+++ public/static/js/exit/exit_ta.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    var isF5 = false;
+    window.document.onkeydown = function() {
+        if (event.keyCode==116) {
+            isF5 = true;
+        }
+    }
+    window.onbeforeunload = function() {
+        console.log('!isF5:'+!isF5);
+        if (!isF5) {
+            return '关闭';
+            $.ajax({
+                url: '{:Url("Home/logout_admin")}'
+            });
+            
+        }
+    }
+});
