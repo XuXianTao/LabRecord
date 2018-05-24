ba84dec460458e36bf2d6da2eb26f5f24e20b3ad
XXT <ku8834367@163.com>
2018-05-24 12:12:40 +0800

    需求更新(ps附实现,待讨论)+学生回车登录+关闭浏览器debug未果

--- public/static/js/exit/exit_stu.js
+++ public/static/js/exit/exit_stu.js
@@ -1,9 +1,19 @@
 $(document).ready(function() {
-    window.onbeforeunload = function() {
-        if(event.clientX>document.body.clientWidth&&event.clientY<0 || event.altKey){ 
+    window.onbeforeunload = function(event) {
+        if(event.clientY<0 || event.altKey) {
+            return '确认登出？';
             $.ajax({
+                type: 'POST',
                 url: '{:Url("Home/logout_stu")}'
             });
         }
+        console.log(event.clientY);
+        console.log(event.altKey);
     }
 });
+function ccc(){
+    console.log('Y:'+event.clientY);
+    console.log('altKey'+event.altKey);
+}
+$(window).keydown(ccc);
+$(window).click(ccc);
