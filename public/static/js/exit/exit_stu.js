91d9ad7ea7b72703a4ac6290734d72d89a9d9a40
WuRui <261857860@qq.com>
2018-05-25 13:39:15 +0800

    updt
    去除exit_stu.js与exit_ta.js的引入。

--- public/static/js/exit/exit_stu.js
+++ public/static/js/exit/exit_stu.js
@@ -1,13 +1,4 @@
 $(document).ready(function() {
-<<<<<<< HEAD
-    window.onbeforeunload = function() {
-        console.log(event.clientX);
-        console.log(event.clientY);
-        return this.event.clientX;
-        alert(this.event.clientX);
-        if(event.clientX>document.body.clientWidth&&event.clientY<0 || event.altKey){ 
-            $.post('{:Url("Home/logout_stu")}');
-=======
     window.onbeforeunload = function(event) {
         if(event.clientY<0 || event.altKey) {
             return '确认登出？';
@@ -15,7 +6,6 @@ $(document).ready(function() {
                 type: 'POST',
                 url: '{:Url("Home/logout_stu")}'
             });
->>>>>>> 46cff3c3dd67eff30318b9ec5a58edf5d7e038ea
         }
         console.log(event.clientY);
         console.log(event.altKey);
